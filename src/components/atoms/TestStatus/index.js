5af517263d56e70c939f09dd3e9b2e0d7f4f617d
Saq65 <saklainshaikh974@gmail.com>
2025-03-26 14:39:50 +0530

    changing status button width

--- src/components/atoms/TestStatus/index.js
+++ src/components/atoms/TestStatus/index.js
@@ -8,32 +8,40 @@ const TestStatus = ({ status }) => {
             case 'finished':
                 return {
                     label: 'Completed',
-                    sx: { backgroundColor: '#16A34A', color: '#FFFFFF',width:'70%' }, // Custom green color
+                    sx: { backgroundColor: '#16A34A', color: '#FFFFFF',maxWidth: '100px',
+                        width: '100%'}, // Custom green color
                 };
             case 'pending':
                 return {
                     label: 'Pending',
-                    sx: { backgroundColor: '#64748B', color: '#FFFFFF' ,width:'70%' }, // Custom grey-blue color
+                    sx: { backgroundColor: '#64748B', color: '#FFFFFF',maxWidth: '100px',
+                        width: '100%'}, // Custom grey-blue color
                 };
             case 'onGoing':
                 return {
                     label: 'Ongoing',
-                    sx: { backgroundColor: '#0284C7', color: '#FFFFFF' ,width:'70%' }, // Custom amber color
+                    sx: {
+                        backgroundColor: '#0284C7', color: '#FFFFFF', maxWidth: '220px',
+                        width: '100%',
+                    }, // Custom amber color
                 };
             case 'expired':
                 return {
                     label: 'Expired',
-                    sx: { backgroundColor: '#DC2626', color: '#FFFFFF',width:'70%'  }, // Custom amber color
+                    sx: { backgroundColor: '#DC2626', color: '#FFFFFF',maxWidth: '100px',
+                        width: '100%', }, // Custom amber color
                 };
             case 'disQualified':
                 return {
                     label: 'Disqualified',
-                    sx: { backgroundColor: '#F97316', color: '#FFFFFF' ,width:'70%' }, // Custom amber color
+                    sx: { backgroundColor: '#F97316', color: '#FFFFFF',maxWidth: '100px',
+                        width: '100%' }, // Custom amber color
                 };
             default:
                 return {
                     label: 'Unknown',
-                    sx: { backgroundColor: '#D1D5DB', color: '#000000' ,width:'70%' }, // Default grey color
+                    sx: { backgroundColor: '#D1D5DB', color: '#000000',maxWidth: '100px',
+                        width: '100%'}, // Default grey color
                 };
         }
     };
