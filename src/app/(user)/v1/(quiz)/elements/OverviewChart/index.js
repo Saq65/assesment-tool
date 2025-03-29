a3130b4a78e99798880f342c2d2fdb723748fa8a
Saq65 <saklainshaikh974@gmail.com>
2025-03-29 17:14:18 +0530

    some changes i did

--- src/app/(user)/v1/(quiz)/elements/OverviewChart/index.js
+++ src/app/(user)/v1/(quiz)/elements/OverviewChart/index.js
@@ -47,7 +47,7 @@ const OverviewChart = () => {
             `&nbsp;Unattempted`,
             `&nbsp;Revisit Later`
         ],
-        colors: ['gray', '#DC2626', '#F97316'],
+        colors: ['green', '#DC2626', '#F97316'],
         legend: {
             show: false,
             fontSize: '16px',
